2113aff9d2a68fef6ae5acfd9927568296ecb853
Matt Wozniak <wozniamj01@gmail.com>
2023-07-08 19:53:36 -0400

    adding comments

--- ToolRentalPOS/src/main/java/com/toolRental/pos/models/Holiday.java
+++ ToolRentalPOS/src/main/java/com/toolRental/pos/models/Holiday.java
@@ -1,21 +1,40 @@
 package com.toolRental.pos.models;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.DayOfWeek;
 
 public class Holiday {
 
+    /**
+     * Checks if the given date is a holiday.
+     *
+     * @param date the date to check
+     * @return true if the date is a holiday, false otherwise
+     */
     public static boolean isHoliday(LocalDate date) {
         if (isIndependenceDay(date) || isLaborDay(date)) {
             return true;
         }
         return false;
     }
-    
+
+    /**
+     * Checks if the given date is Independence Day (July 4th).
+     *
+     * @param date the date to check
+     * @return true if the date is Independence Day, false otherwise
+     */
     private static boolean isIndependenceDay(LocalDate date) {
         return date.getMonth() == Month.JULY && date.getDayOfMonth() == 4;
     }
-    
+
+    /**
+     * Checks if the given date is Labor Day (first Monday in September).
+     *
+     * @param date the date to check
+     * @return true if the date is Labor Day, false otherwise
+     */
     private static boolean isLaborDay(LocalDate date) {
         if (date.getMonth() == Month.SEPTEMBER && date.getDayOfWeek() == DayOfWeek.MONDAY && date.getDayOfMonth() <= 7) {
             return true;
--- ToolRentalPOS/src/main/java/com/toolRental/pos/models/Tool.java
+++ ToolRentalPOS/src/main/java/com/toolRental/pos/models/Tool.java
@@ -25,30 +25,65 @@ public class Tool {
         this.holidayCharge = holidayCharge;
     }
 
+    /**
+     * Retrieves the tool code.
+     *
+     * @return the tool code
+     */
     public String getToolCode() {
         return toolCode;
     }
 
+    /**
+     * Retrieves the tool type.
+     *
+     * @return the tool type
+     */
     public String getToolType() {
         return toolType;
     }
 
+    /**
+     * Retrieves the brand of the tool.
+     *
+     * @return the brand of the tool
+     */
     public String getBrand() {
         return brand;
     }
 
+    /**
+     * Retrieves the daily rental charge for the tool.
+     *
+     * @return the daily rental charge
+     */
     public BigDecimal getDailyCharge() {
         return dailyCharge;
     }
 
+    /**
+     * Checks if the tool has weekday charge.
+     *
+     * @return true if the tool has weekday charge, false otherwise
+     */
     public boolean isWeekdayCharge() {
         return weekdayCharge;
     }
 
+    /**
+     * Checks if the tool has weekend charge.
+     *
+     * @return true if the tool has weekend charge, false otherwise
+     */
     public boolean isWeekendCharge() {
         return weekendCharge;
     }
 
+    /**
+     * Checks if the tool has holiday charge.
+     *
+     * @return true if the tool has holiday charge, false otherwise
+     */
     public boolean isHolidayCharge() {
         return holidayCharge;
     }
